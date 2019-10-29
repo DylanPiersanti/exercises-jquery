4e05e74eb0598d04985aafaad47a83a1aba2e8ba
Piersanti dylan <dylngae@outlook.com>
2019-10-29 11:22:00 +0100

    Change active list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 $(function() {
   $('#navbarDropdown').click(function() {
     $(".dropdown-menu").toggle()
-  })  
+  })  // menu dropDown
 
-  $('a').removeClass('disabled')
+  $('a').removeClass('disabled') // supprime class disabled
 
   $('[name=search]').keyup(function() {
-    $('#searchResult').html($('[name=search]').val())
-  })
+    $('#searchResult').html($(this).val())
+  }) // keyUp
+
+  $('#replaceTitle').click(function() {
+    $('.display-4').text("Web Developer " + Math.floor(Math.random() * 51))
+  }) // replaceTitle + Math.random()
+
+  $('.list-group-item').click(function() {
+    $(this).addClass('active').siblings().removeClass('active')
+  }) // change active list
+
 });
